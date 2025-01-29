31a1c8ef70932bd83229538cf0c8280734c71abb
Tilek2304 <esenaliev2304@gmail.com>
2025-01-29 10:03:04 +0600

    завершил, готов к заливке на сервер

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from pages import views 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_page, name='index'),  
-    path('physics/', views.physics_page, name='physics'),
-    # path('chemistry/', views.chemistry_page, name='list'),
-    # path('biology/', views.biology_page, name='list'),
-    # path('about/', views.about_page, name='list'),
+    path('', views.home_page, name='home'),
+    path('physics/', views.subject_files_page, {'subject': 'physics'}, name='physics'),
+    path('chemistry/', views.subject_files_page, {'subject': 'chemistry'}, name='chemistry'),
+    path('biology/', views.subject_files_page, {'subject': 'biology'}, name='biology'),
 ]
--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
 from .models import SubjectFile
 
-
 def home_page(request):
     return render(request, 'pages/index.html')
 
-# def about_page(request):
-#     return render(request, 'pages/about.html')
-def physics_page(request):
-    files = SubjectFile.objects.filter(subject='physics')
-    return render(request, 'pages/list.html', {'files': files})
-
-# def chemistry_page(request):
-#     files = SubjectFile.objects.filter(subject='chemistry')
-#     return render(request, 'pages/chemistry.html', {'files': files})
-
-# def biology_page(request):
-#     files = SubjectFile.objects.filter(subject='biology')
-#     return render(request, 'pages/biology.html', {'files': files})
+def subject_files_page(request, subject):
+    # Проверяем, что subject есть в списке допустимых значений
+    valid_subjects = [choice[0] for choice in SubjectFile.SUBJECT_CHOICES]
+    if subject not in valid_subjects:
+        return render(request, 'pages/404.html')  # Страница для ошибки 404
+    
+    files = SubjectFile.objects.filter(subject=subject)
+    context = {
+        'files': files,
+        'subject_name': dict(SubjectFile.SUBJECT_CHOICES)[subject],  # Название предмета (на русском)
+        'subject': subject  # Передаем subject для использования в шаблоне (напр., для изображений)
+    }
+    return render(request, 'pages/list.html', context)
