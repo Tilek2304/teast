1035c1ddfd094bc275b786d34e7f240ec8ee52d0
daniiarkg <daniiar.commerce@gmail.com>
2025-02-05 15:04:29 +0500

    ready to commit

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -3,6 +3,6 @@ from .models import SubjectFile
 
 @admin.register(SubjectFile)
 class SubjectFileAdmin(admin.ModelAdmin):
-    list_display = ['subject', 'file', 'uploaded_at']
+    list_display = ['name', 'subject', 'uploaded_at']
     list_filter = ['subject']
-    search_fields = ('file',)  
+    search_fields = ('name',)  
--- mysite/pages/models.py
+++ mysite/pages/models.py
@@ -7,8 +7,11 @@ class SubjectFile(models.Model):
         ('biology', 'Биология'),
     ]
 
+    photo = models.ImageField(null=True, upload_to='main', verbose_name='Фото')
+    name = models.CharField(max_length=250, null=True, blank=True,verbose_name='Название')
     subject = models.CharField(max_length=20, choices=SUBJECT_CHOICES, verbose_name="Предмет")
     file = models.FileField(upload_to='uploads/', verbose_name="ZIP файл")
+    description = models.TextField(null=True, verbose_name='Описание')
     uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата загрузки")
 
     def __str__(self):
